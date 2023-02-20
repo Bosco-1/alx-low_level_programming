f36d19b3bf63d2bfe870867d987367deca989447
Bosco-1 <you@example.com>
2023-02-20 13:39:20 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio>
+/**
+ * main - An algorithm for printing a line using printf
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+	return (0);
+}
