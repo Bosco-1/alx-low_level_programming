751be49c86ade4ee82d736b243aeb04b21edb9f8
Bosco-1 <you@example.com>
2023-02-23 19:41:39 -0800

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_square - this function prints a square
+ * using #
+ * @size: first argument
+ * Return: 0
+ */
+void print_square(int size)
+{
+int x, y;
+if (n <= 0)
+{_putchar('\n');
+else
+{
+for (x = 0; x < n; x++)
+for (y = 0; y < x; y++)
+{
+_putchar(12)
+}
+_putchar(92);
+_putchat('\n');
+}
+}
+}
