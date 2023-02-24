0c73967a420112d549b1a1726e5b0808afa79603
Bosco-1 <122940391+Bosco-1@users.noreply.github.com>
2023-02-24 03:45:53 +0000

    Update 7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,56 +1,25 @@
-
-Product
-Solutions
-Open Source
-Pricing
-Search
-Sign in
-Sign up
-michaelEmeka
-/
-alx-low_level_programming
-Public
-Code
-Issues
-Pull requests
-Actions
-Projects
-Security
-Insights
-alx-low_level_programming/0x04-more_functions_nested_loops/7-print_diagonal.c
-@michaelEmeka
-michaelEmeka Modify 7
-Latest commit dc6749c on Jul 18, 2022
- History
- 1 contributor
-29 lines (27 sloc)  403 Bytes
-
 #include "main.h"
 
 /**
- * print_diagonal - main function
- * @n: this is the function's argument
- * Description: this program draws a diagonal line on the terminal
- * Return: nil
+ * print_square - this function prints a square
+ * using #
+ * @size: first argument
+ * Return: 0
  */
-void print_diagonal(int n)
+void print_square(int size)
 {
-	int s = n, c;
-
-	if (n > 0)
-	{
-		while (n--)
-		{
-			for (c = 0; c < s - (n + 1); c++)
-			{
-				_putchar(' ');
-			}
-			_putchar('\\');
-			_putchar('\n');
-		}
-	}
-	else
-	{
-		_putchar('\n');
-	}
+int x, y;
+if (n <= 0)
+{_putchar('\n');
+else
+{
+for (x = 0; x < n; x++)
+for (y = 0; y < x; y++)
+{
+_putchar(12)
+}
+_putchar(92);
+_putchat('\n');
+}
+}
 }
